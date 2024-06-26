ba7cd8a72b4f6913156bf327599f4b61a0d3678b
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-06-26 12:57:17 +0300

    add Foreground color

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class MyFrame extends JFrame {
@@ -15,6 +16,11 @@ public class MyFrame extends JFrame {
         button.setText("Send");
         button.setFocusable(false);
         button.setIcon(icon);
+        button.setHorizontalTextPosition(JButton.CENTER);
+        button.setVerticalTextPosition(JButton.BOTTOM);
+        button.setFont(new Font("Comic Sans",Font.BOLD,25));
+        button.setIconTextGap(-5);
+        button.setForeground(Color.cyan);
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
